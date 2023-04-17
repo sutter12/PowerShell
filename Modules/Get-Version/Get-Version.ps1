4cb8de181270a1dd8694a6fc24d38fbef240c9b7
Alex Sutter <suttera12@gmail.com>
2023-04-16 23:34:17 -0400

    Update 04/16/2023

---
+++ Modules/Get-Version/Get-Version.ps1
@@ -0,0 +1,47 @@
+
+<#PSScriptInfo
+
+.VERSION 1.0.0
+
+.GUID 8b14b040-01b8-400b-9c84-d4208e697d23
+
+.AUTHOR Alexander Sutter
+
+.COMPANYNAME SutterStudios
+
+.COPYRIGHT
+
+.TAGS
+
+.LICENSEURI
+
+.PROJECTURI
+
+.ICONURI
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS
+
+.EXTERNALSCRIPTDEPENDENCIES
+
+.RELEASENOTES
+Created: 12/12/2022
+
+.PRIVATEDATA
+
+#> 
+
+<# 
+
+.DESCRIPTION 
+ Get the current PS Version Table 
+
+#> 
+
+# PowerShell Version: 5.1
+# Get-Version.ps1
+
+function Get-Version {
+    $PSVersionTable
+} # end Get-Version
